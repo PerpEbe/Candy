393883b33a299441db6ae81f8d77b65f4433f404
PerpEbe <ebenezerperplex010@gmail.com>
2024-07-25 10:05:47 +0300

    1005

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -7,7 +7,8 @@ const NewCollections = () => {
   const [new_collection, setNew_collection] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:4000/newcollections")
+    // fetch("http://localhost:4000/newcollections")
+    fetch("https://candy-wnwv.onrender.com/newcollections")
       .then((response) => response.json())
       .then((data) => setNew_collection(data));
   }, []);
